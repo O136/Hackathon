2315e8840df3cef92e5876eb3a3f76652dcb5673
Oleg Patraschku <oleg.patraschku@gmx.de>
2016-11-12 14:22:53 +0100

    now we got it !

--- blog.py
+++ blog.py
@@ -15,14 +15,6 @@ def blog_index():
     return None
 
 
-@bottle.route('/create_trip', method='POST')
-def create_trip():
-    bottle.response.content_type = 'application/json'
-    data = dumps(bottle.request.json)
-    print data
-    return 'ok'
-
-
 @bottle.route('/create_user', method='POST')
 def create_user():
     collection = db['Users']
@@ -38,8 +30,11 @@ def get_user():
     collection = db['Users']
     bottle.response.content_type = 'application/json'
     data = bottle.request.json
-    obj = collection.find_one({'email': data['email']})
-    return dumps(obj)
+    cursor = collection.find_one({'email': data['email']})
+    if cursor is None:
+        obj_id = collection.insert_one({'email': data['email']})
+        cursor = collection.find_one({'_id': obj_id.inserted_id})
+    return dumps(cursor)
 
 
 @bottle.route('/create_trip', method='POST')
@@ -47,38 +42,98 @@ def create_trip():
     collection = db['Trip']
     bottle.response.content_type = 'application/json'
     data = bottle.request.json
-
     try:
-        name = data['name']
-        sdate = data['sdate']
-        edate = data['edate']
-        source = data['source']
-        dest = data['destination']
-        users = data['users']
-        activities = data['activities']
-        obj = collection.insert_one()
+
+        try:
+            collection2 = db['SuggestedActivities']
+            all_activities = []
+            for i in collection2.find({'name': {'$in': data['activities']}}):
+                all_activities.append(i)
+
+            data["activities"] = all_activities
+        except:
+            pass
+        collection.insert_one(data)
     except:
-        print('Error')
-        return None
+        pass
+    obj = collection.find_one({'name': data['name']})
 
     return dumps(obj)
 
 
-@bottle.route('/get_trip', method='POST')
-def get_trip():
+@bottle.route('/get_trips', method='POST')
+def get_trips():
     collection = db['Trip']
     bottle.response.content_type = 'application/json'
     data = bottle.request.json
+    obj = collection.find(data)
+    return dumps(obj)
+
+
+@bottle.route('/get_trips_by_persons', method='POST')
+def get_trips():
+    collection = db['Trip']
+    bottle.response.content_type = 'application/json'
+    data = bottle.request.json
+    obj = collection.find({'persons': {'$in': [data['email']]}}) #fara paranteze
+    return dumps(obj)
 
 
 @bottle.route('/create_activity', method='POST')
 def create_activity():
-    pass
+    collection = db['Activity']
+    bottle.response.content_type = 'application/json'
+    data = bottle.request.json
+    obj = collection.insert_one(data)
+    return dumps(obj)
 
 
 @bottle.route('/get_activity', method='POST')
 def get_activity():
-    pass
+    collection = db['Activity']
+    bottle.response.content_type = 'application/json'
+    data = bottle.request.json
+    obj = collection.find_one(data)
+    return dumps(obj)
+
+
+@bottle.route('/update_activity', method='POST')
+def update_up():
+    collection = db['Trip']
+    bottle.response.content_type = 'application/json'
+    data = bottle.request.json
+    obj = collection.find_one({'name' : data['name'], 'ups': {'$exists': True}})
+
+   # db.Trip.update({name: "Hollywood"}, {$push:{"activities": {"name": "shooting", "ups": ["oleska"]}}})
+
+
+    if obj is not None: # if it has ups/downs update it, else
+        db['Trip'].update({'name': data['trip_name']}, {'$push':{'activities': { 'name': data['name'], 'ups': [data['user']]}}})
+    else:
+        pass
+
+    return dumps(obj)
+
+
+@bottle.route('/get_suggested_activities', method='POST')
+def get_suggested_activities():
+    collection = db['SuggestedActivities']
+    bottle.response.content_type = 'application/json'
+    data = bottle.request.json
+    obj = collection.find({'destination': data['destination']})
+    return dumps(obj)
+
+
+@bottle.route('/link_suggested_activities', method='POST')
+def link_suggested_activities():
+    collection = db['Trip']
+    bottle.response.content_type = 'application/json'
+    data = bottle.request.json
+    obj = collection.find({'destination': data['destination']})
+    return dumps(obj)
+
+
+
 
 bottle.debug(True)
 bottle.run(host='131.159.192.152', port=8082)  # Start the webserver, listens
